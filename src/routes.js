b2c4afb85ab3629fd7e3699e7a614fb64ab21e73
Dev-Hood <estudartagc@gmail.com>
2021-12-30 22:10:31 -0300

    iniciando requisições

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,23 @@
 const express = require('express')
-const UserController = require('./controllers/UserController')
+const bodyparser = require('body-parser')
+const UserController = require('./controllers/UserController');
 const routes = express.Router();
+const Users = require('./models/Users')
+routes.use(bodyparser.urlencoded({extended:false}))
+routes.use(bodyparser.json())
+
+routes.get('/login',(req,res)=>{
+    res.sendFile(__dirname + '/public/login.html')
+})
+routes.post('/login', async (req,res)=>{
+    const email = req.body.email
+    const user = await Users.findOne({where:{email}})
+    console.log(user)
+    /*if(req.body.email == 'ta@gmail.com'){
+        res.sendFile(__dirname+'/dashboard/dashboard.html')
+    }*/
+})
+
 
 routes.post('/user',UserController.store)
 
--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express()
 const routes = require('./routes')
 require('./database')
+app.use(express.static(__dirname+'/public'))
+app.use(express.static(__dirname+'/dashboard'))
 app.use(express.json())
 app.use(routes)
 app.listen(3333);
